6a03754568432d173a34f96d9bef0c5ea78bae4c
guoyuxue <1490359123@qq.com>
2018-11-16 12:03:03 +0800

    新闻资讯的查询

--- qh-web/src/main/java/com/ola/qh/util/Patterns.java
+++ qh-web/src/main/java/com/ola/qh/util/Patterns.java
@@ -3,7 +3,7 @@ package com.ola.qh.util;
 public class Patterns {
 
 	//////新闻分页的个数
-    public static final int zupageSize = 12;
+    public static final int zupageSize = 6;
     
     public static final String INTERNAL_MOBILE_PATTERN = "^((13)|(14)|(15)|(16)|(17)|(18)|(19))\\d{9}$";
 
