9a268a01e4a73758143204c27a2669437dd2e71a
Administrator <Administrator@192.168.1.4>
2018-11-16 08:43:51 +0800

    修改注释

--- qh-web/src/main/java/com/ola/qh/service/imp/BannerService.java
+++ qh-web/src/main/java/com/ola/qh/service/imp/BannerService.java
@@ -8,7 +8,15 @@ import org.springframework.stereotype.Service;
 import com.ola.qh.dao.BannerDao;
 import com.ola.qh.entity.Banner;
 import com.ola.qh.service.IBannerService;
-
+/**
+ * 
+ * 
+* @ClassName: BannerService
+* @Description:  BannerService接口实现类
+* @author guozihan
+* @date   2018年11月15日
+*
+ */
 @Service
 public class BannerService implements IBannerService {
 
