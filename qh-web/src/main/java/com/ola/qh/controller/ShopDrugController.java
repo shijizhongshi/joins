9b845d4a2fa9b75c5ff78f83469068e71c52e453
guoyuxue <1490359123@qq.com>
2018-12-29 17:31:53 +0800

    修改参数

--- qh-web/src/main/java/com/ola/qh/controller/ShopDrugController.java
+++ qh-web/src/main/java/com/ola/qh/controller/ShopDrugController.java
@@ -87,6 +87,7 @@ public class ShopDrugController {
 			@RequestParam(name="shopId",required=false)String shopId,
 			@RequestParam(name="status",required=false)int status,
 			@RequestParam(name="drugName",required=false)String drugName,
+			@RequestParam(name="istimes",required=false)int istimes,
 			@RequestParam(name="categorySubname",required=false)String categorySubname,
 			@RequestParam(name="categoryName",required=false)String categoryName,
 			@RequestParam(name="ordersSales",required=false)int ordersSales,
@@ -105,6 +106,7 @@ public class ShopDrugController {
 		sdd.setOrdersSales(ordersSales);
 		sdd.setOrdersprice(ordersprice);
 		sdd.setDrugName(drugName);
+		sdd.setIstimes(istimes);
 		return shopDrugService.selectDrugList(sdd);
 	}
 	/**
