bc200c670de9b1ac30164a075af8b9527b47e09b
llg <917968474@qq.com>
2019-04-10 14:51:13 +0800

    测试库连接地址变更/主讲老师+老师集合拼接返回

--- qh-web/src/main/java/com/ola/qh/dao/CourseClassDao.java
+++ qh-web/src/main/java/com/ola/qh/dao/CourseClassDao.java
@@ -12,21 +12,24 @@ import com.ola.qh.vo.CourseClassDomain;
 public interface CourseClassDao {
 
 	public List<CourseNofree> nofreeList(CourseClassDomain ccd);
-		
-		public CourseNofree nofreeSingle(@Param("id") String id);
-		
-		public List<CourseClass> classList(CourseClassDomain ccd);
-		
-		public CourseClass classSingle(@Param("id") String id);
-		
-		public List<CourseTeacher> teacherList(@Param("classId")String classId);/////根据课程的id查老师的集合
-		
-		public int teacherCount(@Param("classId") String classId);////老师的总个数
-		
-		public int ordersCount(@Param("productId") String productId);/////查课程订单的个数
-		
-		//////通过name查教师的信息
-		public String getTeacherImg(@Param("name")String name);
-		
-		
+
+	public CourseNofree nofreeSingle(@Param("id") String id);
+
+	public List<CourseClass> classList(CourseClassDomain ccd);
+
+	public CourseClass classSingle(@Param("id") String id);
+
+	public List<CourseTeacher> teacherList(@Param("classId") String classId,
+			@Param("courseLecturer") String courseLecturer);///// 根据课程的id查老师的集合， 并且排除主讲老师
+
+	public List<CourseTeacher> selectCourseLecturer(@Param("classId") String classId,
+			@Param("courseLecturer") String courseLecturer);//根据课程id 主讲老师名字查询集合
+
+	public int teacherCount(@Param("classId") String classId);//// 老师的总个数
+
+	public int ordersCount(@Param("productId") String productId);///// 查课程订单的个数
+
+	////// 通过name查教师的信息
+	public String getTeacherImg(@Param("name") String name);
+
 }
