c4a0c6f4c4b51218ec6ffc8c73c9ff5d17a1ee52
llg <917968474@qq.com>
2019-05-27 16:16:08 +0800

    购买课程接口添加了每个课程下的章节数统计/章集合返回二级专业name

--- qh-web/src/main/java/com/ola/qh/entity/UserBuyCourse.java
+++ qh-web/src/main/java/com/ola/qh/entity/UserBuyCourse.java
@@ -1,42 +1,64 @@
 package com.ola.qh.entity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 /**
  * 
  * 
-* @ClassName: UserBuyCourse  
-* @Description: TODO(这里用一句话描述这个类的作用)  
-* @author guoyuxue  
-* @date 2018年11月29日  
-*
+ * @ClassName: UserBuyCourse
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author guoyuxue
+ * @date 2018年11月29日
+ *
  */
 public class UserBuyCourse {
 
-	private String userId;///用户的id
-	
+	private String userId;/// 用户的id
+
 	private String id;
-	
+
 	private String courseId;
-	
-	private String courseName;////
-	
-	private BigDecimal courseDiscountPrice;////课程的折扣价
-	
-	private BigDecimal coursePrice;////课程的原价
-	
-	private String courseImgUrl;////课程的图片
-	
+
 	private String classId;
-	
+
+	private String courseName;////
+
+	private BigDecimal courseDiscountPrice;//// 课程的折扣价
+
+	private BigDecimal coursePrice;//// 课程的原价
+
+	private String courseImgUrl;//// 课程的图片
+
 	private String ordersId;
-	
+
 	private String showtime;
-	
+
 	private Date addtime;///
-	
-	private String payType;///线上支付或者是线下支付的
-	
+
+	private String payType;/// 线上支付或者是线下支付的
+
+	private List<Course> listcourse = new ArrayList<Course>();
+
+	private Integer classChapterSize;
+
+	public Integer getClassChapterSize() {
+		return classChapterSize;
+	}
+
+	public void setClassChapterSize(Integer classChapterSize) {
+		this.classChapterSize = classChapterSize;
+	}
+
+	public List<Course> getListcourse() {
+		return listcourse;
+	}
+
+	public void setListcourse(List<Course> listcourse) {
+		this.listcourse = listcourse;
+	}
 
 	public String getPayType() {
 		return payType;
@@ -133,7 +155,5 @@ public class UserBuyCourse {
 	public void setShowtime(String showtime) {
 		this.showtime = showtime;
 	}
-	
-	
-	
+
 }
